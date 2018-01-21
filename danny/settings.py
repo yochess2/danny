6a3f083c5940a9e01d1d7cafd5b81cdc6f82306c
Daham Drake Wang <drakewang@gmail.com>
2018-01-20 17:29:06 -0800

    coccrect host

--- danny/settings.py
+++ danny/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = DANNY_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['calm-springs-32281', 'localhost']
+ALLOWED_HOSTS = ['calm-springs-32281.herokuapp.com', 'localhost']
 
 
 # Application definition
